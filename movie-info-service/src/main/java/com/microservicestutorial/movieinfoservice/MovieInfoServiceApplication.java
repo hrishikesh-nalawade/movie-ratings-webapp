cce0e0c0e4ac0a082a3915574c19f408d82f7eeb
Hrishikesh Nalawade <hrishikesh.nalawade.17@gmail.com>
2024-05-11 02:40:17 +0530

    movie info service eureka client code

--- movie-info-service/src/main/java/com/microservicestutorial/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/microservicestutorial/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,10 +2,9 @@ package com.microservicestutorial.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-//@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
